aae9d0d28fcf06ff5202fcf9741a19d426957358
itsAanis <aanishassan@hotmail.com>
2022-08-28 13:29:22 +0100

    final MVC changes

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -24,3 +24,39 @@ module.exports.post_main = (req, res) => {
         console.log(err)
     })
     }
+
+module.exports.get_edit = (req,res) => {
+    const id = req.params.id;
+    Todos.find()
+    .then(result => {
+        res.render('edit.ejs',{
+            todoTask: result, idTask: id})
+    })
+
+    }
+
+   module.exports.put_edit = (req,res) => {
+        const {data, title} = req.body
+        console.log(req.body)
+        const id =req.params.id
+        console.log(id)
+        Todos.findByIdAndUpdate(id, {
+            content: data,
+            title : title
+        }).then((result) =>{
+       return res.status(201).json({redirect:'/'})
+    })
+    .catch(err => {
+        res.status(500).send(err)})
+    }
+    
+    module.exports.get_delete = (req, res) => {
+        const id = req.params.id
+        Todos.findByIdAndRemove(id)
+        .then(result => {
+            res.redirect('/')
+        })
+        .catch(err => {
+            res.status(500).send({err: 'could not delete the to-do'})
+        })    
+    }
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -5,69 +5,12 @@ const homeController = require("../controllers/homeController")
 const Todos = require("../models/todotasks")
 
 
-
 router.get('/', homeController.get_main)
-
 router.post('/', homeController.post_main)
+router.get("/edit/:id", homeController.get_edit)
+router.put("/edit/:id", homeController.put_edit)
+router.get("/remove/:id", homeController.get_delete)
 
 
-
-router
-.route("/edit/:id")
-.get((req,res) => {
-const id = req.params.id;
-Todos.find()
-.then(result => {
-    res.render('edit.ejs',{
-        todoTask: result, idTask: id})
-})
-}) 
-
-
-
-router.put("/edit/:id",(req,res) => {
-    const {data, title} = req.body
-    console.log(req.body)
-    const id =req.params.id
-    console.log(id)
-    Todos.findByIdAndUpdate(id, {
-        content: data,
-        title : title
-    }).then((result) =>{
-   return res.status(201).json({redirect:'/'})
-})
-.catch(err => {
-    res.status(500).send(err)})
-})
-
-    /*
-    const id =req.params.id
-    Todos.findByIdAndUpdate(id, {
-        title: req.body.title,
-        content: req.body.content
-    })
-    .then(result => {
-        res.status(200).redirect('/')
-    })
-    .catch(err => {
-    res.status(500).send(err)
-  
-    })
-}) */
-
-
-
-router
-.route("/remove/:id")
-.get((req, res) => {
-    const id = req.params.id
-    Todos.findByIdAndRemove(id)
-    .then(result => {
-        res.redirect('/')
-    })
-    .catch(err => {
-        res.status(500).send({err: 'could not delete the to-do'})
-    })    
-})
 
 module.exports = router
